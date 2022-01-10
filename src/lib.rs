b5e03758afca4c8572f71f4fdaffc2ef0a1ca3db
David Thureson <David.G.Thureson@gmail.com>
2022-01-10 13:16:28 -0800

    Word Search: Coded placing the first word and printing the puzzle and debug info.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
 pub use util::*;
 
 pub mod word_search;
+
+pub use itertools::Itertools;
--- src/word_search/puzzle.rs
+++ src/word_search/puzzle.rs
@@ -4,11 +4,15 @@ use std::time::Instant;
 use rand::{thread_rng, Rng};
 use rand::seq::SliceRandom;
 use std::collections::BTreeMap;
+use std::fmt::{Display, Formatter};
+use itertools::Itertools;
 
 const GRID_SIZE_MAX: usize = 30;
 const CREATE_SECONDS_MAX: usize = 10;
+const NO_CHAR: char = '-';
 
 type Grid = Vec<Vec<Cell>>;
+type Offset = [isize; 2];
 
 #[derive(Clone)]
 pub struct Puzzle {
@@ -20,6 +24,7 @@ pub struct Puzzle {
     pub y_min: usize,
     pub y_max: usize,
     pub placements: BTreeMap<String, Placement>,
+    pub char_map: BTreeMap<char, Vec<Position>>,
 }
 
 #[derive(Clone)]
@@ -33,10 +38,16 @@ pub struct Cell {
 
 #[derive(Clone)]
 pub struct Placement {
-    pub position: (usize, usize),
+    pub position: Position,
     pub direction: Direction,
     pub intersection_count: usize,
     pub size_required: usize,
+}
+
+#[derive(Clone)]
+pub struct Position {
+    x: usize,
+    y: usize,
 }
 
 #[derive(Clone)]
@@ -70,6 +81,7 @@ impl Puzzle {
             y_min,
             y_max,
             placements: Default::default(),
+            char_map: Default::default(),
         }
     }
 
@@ -87,12 +99,15 @@ impl Puzzle {
     }
 
     pub fn find_best_puzzle(words: &Vec<String>) -> Option<Self> {
+        let try_count_max = 10;
+        let mut try_count = 1;
         let start_time = Instant::now();
         let mut best_puzzle = None;
         let mut best_size = usize::max_value();
         loop {
             let mut puzzle = Self::new(words);
             puzzle.create();
+            puzzle.print_all();
             let puzzle_size = puzzle.size;
             if puzzle_size < best_size {
             // let new_is_better = best_puzzle.map_or(true, |best_puzzle: Puzzle| best_puzzle.size > puzzle.size);
@@ -103,6 +118,10 @@ impl Puzzle {
             }
             let elapsed = (Instant::now() - start_time).as_secs();
             if elapsed > CREATE_SECONDS_MAX as u64 {
+                break;
+            }
+            try_count += 1;
+            if try_count > try_count_max {
                 break;
             }
         }
@@ -116,52 +135,193 @@ impl Puzzle {
         self.place_first_word();
 
 
-
     }
 
     fn place_first_word(&mut self) {
         let word = self.words.remove(0);
+        let midpoint = self.get_midpoint();
         let direction = Direction::random();
-        let x_y = match direction {
-            Direction::N => (self.)
-            Direction::NE => {}
-            Direction::E => {}
-            Direction::SE => {}
-            Direction::S => {}
-            Direction::SW => {}
-            Direction::W => {}
-            Direction::NW => {}
+        let position= match direction {
+            Direction::N => Position::new(midpoint.x, self.y_max),
+            Direction::NE => Position::new(self.x_min, self.y_max),
+            Direction::E => Position::new(self.x_min, midpoint.y),
+            Direction::SE => Position::new(self.x_min, self.y_min),
+            Direction::S => Position::new(midpoint.x, self.y_min),
+            Direction::SW => Position::new(self.x_max, self.y_min),
+            Direction::W => Position::new(self.x_max, midpoint.y),
+            Direction::NW => Position::new(self.x_max,self.y_max),
+        };
+        let placement = Placement::new(position, direction, 0, self.size);
+        self.apply_word_placement(word, placement);
+    }
+
+    fn apply_word_placement(&mut self, word: String, placement: Placement) {
+        let offset = placement.direction.get_offset();
+        let mut pos = placement.position.clone();
+        for char in word.chars() {
+            let cell = self.get_cell_mut(&pos);
+            if cell.char != NO_CHAR && cell.char != char {
+                panic!("Trying to place word \"{}\" with {}. Conflicting character at {}: '{}'.",
+                    &word, &placement, &pos, cell.char);
+            }
+            if cell.has_direction_conflict(&placement.direction) {
+                panic!("Trying to place word \"{}\" with {}. Direction conflict at {}.",
+                       &word, &placement, &pos);
+            }
+            let mut add_to_char_map = false;
+            if cell.char == char {
+                // placement.intersection_count += 1;
+            } else {
+                cell.char = char;
+                add_to_char_map = true;
+            }
+            cell.set_direction_flag(&placement.direction);
+            if add_to_char_map {
+                let char_map_entry = self.char_map.entry(char).or_insert(vec![]);
+                char_map_entry.push(pos.clone());
+            }
+            pos.apply_offset(&offset);
+        }
+        self.placements.insert(word, placement);
+    }
+
+    #[inline]
+    fn get_midpoint(&self) -> Position {
+        let x = self.x_min + ((self.x_max - self.x_min) / 2);
+        let y = self.y_min + ((self.y_max - self.y_min) / 2);
+        Position::new(x, y)
+    }
+
+    fn get_cell_mut(&mut self, position: &Position) -> &mut Cell {
+        &mut self.grid[position.y][position.x]
+    }
+
+    pub fn get_char(&self, position: &Position) -> char {
+        self.grid[position.y][position.x].char
+    }
+
+    pub fn get_char_xy(&self, x: usize, y: usize) -> char {
+        self.grid[y][x].char
+    }
+
+    pub fn get_description_line(&self) -> String {
+        format!("Puzzle: word count = {}; size = {}; x_min = {}, x_max = {}, y_min = {}, y_max = {}, placement count = {}, char map size = {}", self.words.len(), self.size, self.x_min, self.x_max, self.y_min, self.y_max, self.placements.len(), self.char_map.len())
+    }
+
+    pub fn print(&self, show_placements: bool, show_char_map: bool, show_puzzle: bool) {
+        println!("\n{}", self.get_description_line());
+        if show_placements {
+            println!("\tPlacements:");
+            for (word, placement) in self.placements.iter() {
+                println!("\t\t\"{}\" at {}.", word, placement);
+            }
+        }
+        if show_char_map {
+            println!("\tChar Map:");
+            for (char, positions) in self.char_map.iter() {
+                let positions_desc = positions.iter().map(|pos| pos.to_string()).join(", ");
+                println!("\t\t\"{}\" at {}.", char, positions_desc);
+            }
+        }
+        if show_puzzle {
+            self.print_puzzle();
         }
     }
 
+    pub fn print_all(&self) {
+        self.print(true, true, true);
+    }
+
+    fn print_puzzle(&self) {
+        println!();
+        for y in self.y_min..=self.y_max {
+            let line = (self.x_min..=self.x_max).into_iter()
+                .map(|x| self.get_char_xy(x, y).to_uppercase())
+                .join(" ");
+            println!("{}", line);
+        }
+        println!();
+    }
 }
 
 impl Cell {
     pub fn new() -> Self {
         Self {
-            char: ' ',
+            char: NO_CHAR,
             has_n_s: false,
             has_ne_sw: false,
             has_e_w: false,
             has_se_nw: false
         }
     }
+
+    pub fn has_direction_conflict(&self, direction: &Direction) -> bool {
+        match direction {
+            Direction::N | Direction::S => self.has_n_s,
+            Direction::NE | Direction::SW => self.has_ne_sw,
+            Direction::E | Direction::W => self.has_e_w,
+            Direction::SE | Direction::NW => self.has_se_nw,
+        }
+    }
+
+    pub fn set_direction_flag(&mut self, direction: &Direction) {
+        match direction {
+            Direction::N | Direction::S => self.has_n_s = true,
+            Direction::NE | Direction::SW => self.has_ne_sw = true,
+            Direction::E | Direction::W => self.has_e_w = true,
+            Direction::SE | Direction::NW => self.has_se_nw = true,
+        }
+    }
+}
+
+impl Display for Cell {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(f, "[Cell: char = '{}'; has_n_s = {}; has_ne_sw = {}; has_e_w = {}; has_se_nw = {}]", self.char, self.has_n_s, self.has_ne_sw, self.has_e_w, self.has_se_nw)
+    }
 }
 
 impl Placement {
-    pub fn new(position: (usize, usize), direction: &Direction, intersection_count: usize, size_required: usize) -> Self {
+    pub fn new(position: Position, direction: Direction, intersection_count: usize, size_required: usize) -> Self {
         Self {
             position,
-            direction: direction.clone(),
+            direction,
             intersection_count,
             size_required
         }
     }
+}
 
+impl Display for Placement {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(f, "[Placement: position = '{}'; direction = {}; intersection_count = {}, size_required = {}]", self.position, self.direction, self.intersection_count, self.size_required)
+    }
+}
+
+impl Position {
+    pub fn new(x: usize, y: usize) -> Self {
+        assert!(x < GRID_SIZE_MAX * 2);
+        assert!(y < GRID_SIZE_MAX * 2);
+        Self {
+            x,
+            y
+        }
+    }
+
+    #[inline]
+    pub fn apply_offset(&mut self, offset: &Offset) {
+        self.x = (self.x as isize + offset[0]) as usize;
+        self.y = (self.y as isize + offset[1]) as usize;
+    }
+}
+
+impl Display for Position {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(f, "({}, {})", self.x, self.y)
+    }
 }
 
 impl Direction {
-    pub fn get_offset(&self) -> [isize; 2] {
+    pub fn get_offset(&self) -> Offset {
         match self {
             Direction::N => [0, -1],
             Direction::NE => [1, -1],
@@ -189,6 +349,22 @@ impl Direction {
         }
     }
 
+}
+
+impl Display for Direction {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        let dir_desc = match self {
+            Direction::N => "N",
+            Direction::NE => "NE",
+            Direction::E => "E",
+            Direction::SE => "SE",
+            Direction::S => "S",
+            Direction::SW => "SW",
+            Direction::W => "W",
+            Direction::NW => "NW",
+        };
+        write!(f, "{}", dir_desc)
+    }
 }
 
 pub fn slice_str_to_strings(list: &[&str]) -> Vec<String> {
